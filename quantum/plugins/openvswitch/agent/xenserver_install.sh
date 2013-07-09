6b5d96e0580cb8127e41055cb510a3adc46739e4
hswayne77 <hswayne77@gmail.com>
2013-07-09 19:12:12 +0800

    Update xenserver_install.sh

--- quantum/plugins/openvswitch/agent/xenserver_install.sh
+++ quantum/plugins/openvswitch/agent/xenserver_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-CONF_FILE=/etc/xapi.d/plugins/ovs_quantum_plugin.ini
+set -x
+
+#CONF_FILE=/etc/xapi.d/plugins/ovs_quantum_plugin.ini
+CONF_FILE=/etc/quantum/plugins/ovs_quantum_plugin.ini
 #VERSION=$(python -c "import sys,os ; sys.path.append('../../../../quantum/') ; import version ; os.chdir('../../../../'); print version.version_info.canonical_version_string()")
 VERSION=2013.1
 
@@ -20,19 +23,30 @@ sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
 
 # install ovs-quantum-agent rpm package
 # --------------------------------------
-rpm -Uvh ../ovs_quantum_agent/build/rpm/RPMS/noarch/ovs-quantum-agent-${VERSION}-1.noarch.rpm
+RPM_FILE=ovs-quantum-agent-${VERSION}-1.noarch.rpm
+if [[ $(rpm -qa 'ovs-quantum-agent') =~ '*quantum-agent*' ]];then
+    rpm -e ${RPM_FILE}
+fi
+rpm -Uvh ../ovs_quantum_agent/build/rpm/RPMS/noarch/${RPM_FILE}
 
 # create integration bridge
 # -------------------------
-xe network-list name-label="integration-bridge" | grep xapi >/dev/null 2>&1
+xe network-list name-label="br-int" | grep xapi >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "No integration bridge found.  Creating."
-    xe network-create name-label="integration-bridge"
+    xe network-create name-label="br-int"
 fi
+
+# copy quantum config
+# -------------------
+QCFG=/etc/quantum
+cp ../../../../etc/quantum.conf ${QCFG}
+cp -r ../../../../etc/quantum/ ${QCFG}
+
 
 # ovs_quantum_plugin config setting
 # ---------------------------------
-BR=$(xe network-list --minimal name-label="integration-bridge" params=bridge)
+BR=$(xe network-list --minimal name-label="br-int" params=bridge)
 CONF_BR=$(grep integration-bridge ${CONF_FILE} | cut -d= -f2)
 if [ "X$BR" != "X$CONF_BR" ]; then
     echo "Integration bridge doesn't match configuration file; fixing."
